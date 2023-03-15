fa661cfa12b507dc1ca9af2658023f857aca7fd6
Jdure <jdure067@gmail.com>
2023-03-15 12:31:11 -0400

    Initial Commit

---
+++ recibo-ios/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  recibo-ios
+//
+//  Created by Jonathan Dure on 2023-03-15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ recibo-ios/recibo_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  recibo_iosApp.swift
+//  recibo-ios
+//
+//  Created by Jonathan Dure on 2023-03-15.
+//
+
+import SwiftUI
+
+@main
+struct recibo_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
